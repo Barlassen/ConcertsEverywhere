a6cc2ab803a24fbb99fa71a0119ce83b673e722d
oktay <oktayy.yuce@gmail.com>
2026-04-21 05:01:37 +0300

    Initial Commit

---
+++ Ticket/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Ticket
+//
+//  Created by Oktay on 21.04.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TicketTests/TicketTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicketTests.swift
+//  TicketTests
+//
+//  Created by Oktay on 21.04.2026.
+//
+
+import Testing
+@testable import Ticket
+
+struct TicketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
